67852b295978e925871174e0927e7514836b1e60
Andreyrvs <cortaesai@gmail.com>
2021-11-26 11:18:05 -0300

    Tentativa Requisito 04

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,10 +2,16 @@ const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  const acha = data.employees.filter((a) => a.managers === id);
+
+  return acha;
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
 }
+
+console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
+// console.log();
 
 module.exports = { isManager, getRelatedEmployees };
