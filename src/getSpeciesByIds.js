b752e5056f154d6c02cea2db787784eba85db11e
Andreyrvs <cortaesai@gmail.com>
2021-11-25 22:09:12 -0300

    Tentativa Requisito 01

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,14 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
-  // apenas
+  let buscaId = [];
+  const { species, id } = data;
+  buscaId = ;
+
+  return buscaId;
 }
+
+console.log(getSpeciesByIds('01422318-ca2d-46b8-b66c-3e9e188244ed'));
 
 module.exports = getSpeciesByIds;
