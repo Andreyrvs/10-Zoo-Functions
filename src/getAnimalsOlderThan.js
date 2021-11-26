6c4e649d698bb2fb95a2a6c43b25049eeedc96da
Andreyrvs <cortaesai@gmail.com>
2021-11-26 10:03:21 -0300

    Tentativa Requisito 02

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,13 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const checkIdade = data.species
+    .find((nomeAnimal) => nomeAnimal.name === animal)
+    .residents.map((idade) => (idade.age > age))
+    .filter((elemento, index, arr) => index === arr.indexOf(elemento));
+  return checkIdade;
 }
+
+console.log(getAnimalsOlderThan('penguins', 10));
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,11 +2,10 @@ const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   // seu código aqui
-  let buscaId = [];
-  const { species, id } = data;
-  buscaId = ;
 
-  return buscaId;
+  const [species] = data.species;
+
+  return console.log(species);
 }
 
 console.log(getSpeciesByIds('01422318-ca2d-46b8-b66c-3e9e188244ed'));
